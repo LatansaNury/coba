b55b1b625b72940b6de099e15a3ece50097132d4
latansa <nuryizza@gmail.com>
2020-07-28 11:30:57 +0700

    fall detection

---
+++ app/src/main/java/com/example/coba/CallFragment.java
@@ -0,0 +1,104 @@
+package com.example.coba;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CallFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class CallFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    //Firebase
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+    TextView txtDetection;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CallFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CallFragment newInstance(String param1, String param2) {
+        CallFragment fragment = new CallFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CallFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_call, container, false);
+        txtDetection = (TextView) view.findViewById(R.id.txtDetection);
+        myRef = database.getInstance().getReference().child("detection");
+        return view;
+        //return inflater.inflate(R.layout.fragment_call, container, false);
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+//        myRef = database.getInstance().getReference().child("detection");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String detection = dataSnapshot.child("sensor/detection").getValue(String.class);
+                //Log.d(TAG, "Value is: " + detection);
+                txtDetection.setText(detection);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.i("Info Error", "Failed to read value.", error.toException());
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/coba/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.coba;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPAger;
+    TabLayout tabLayout;
+
+    private static final String TAG = "fireiot";
+    TextView txtDetection;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
+    private final static String default_notification_channel_id = "default" ;
+
+//    FirebaseDatabase database = FirebaseDatabase.getInstance();
+//    DatabaseReference myRef = database.getReference();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        PagerAdapter.addFragment(new ChatFragment());
+        PagerAdapter.addFragment(new CallFragment());
+        PagerAdapter.addFragment(new StatusFragment());
+
+        viewPAger = findViewById(R.id.viewpager);
+        viewPAger.setAdapter(pagerAdapter);
+
+        tabLayout = findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(viewPAger);
+
+        tabLayout.getTabAt(0).setText("Chat");
+        tabLayout.getTabAt(1).setText("Call");
+        tabLayout.getTabAt(2).setText("Status");
+//        FirebaseMessaging.getInstance().subscribeToTopic("android");
+//        myRef = database.getInstance().getReference().child("detection");
+        widget();
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String detection = dataSnapshot.child("sensor/detection").getValue(String.class);
+//                //Log.d(TAG, "Value is: " + detection);
+//
+//
+//                txtDetection.setText(detection);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.i("Info Error", "Failed to read value.", error.toException());
+//
+//            }
+//
+//
+//
+//        });
+//        myRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                String detection = txtDetection.getText().toString();
+//                if (detection.isEmpty()){
+//
+//                }else {
+//                    createNotification();
+//                }
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+    }
+//    private  void notification(){
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            NotificationChannel channel =
+//                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager manager =
+//                    getSystemService(NotificationManager.class);
+//            manager.createNotificationChannel(channel);
+//        }
+//        NotificationCompat.Builder builder  = new NotificationCompat.Builder(this, "n")
+//                .setContentText("code sphare")
+//                .setSmallIcon(R.drawable.notification_icon)
+//                .setAutoCancel(true)
+//                .setContentText("data added");
+//        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+//        managerCompat.notify(999, builder.build());
+//    }
+//private void createNotification () {
+//    NotificationManager mNotificationManager = (NotificationManager) getSystemService( NOTIFICATION_SERVICE ) ;
+//    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity. this, default_notification_channel_id ) ;
+//    mBuilder.setContentTitle( "Notification Title" ) ;
+//    mBuilder.setContentText( "Content to be added" ) ;
+////    mBuilder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+//    mBuilder.setLargeIcon(BitmapFactory. decodeResource (getResources() , R.drawable. ic_launcher_foreground )) ;
+//    mBuilder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
+//    mBuilder.setAutoCancel( true ) ;
+//    if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+//        int importance = NotificationManager. IMPORTANCE_HIGH ;
+//        NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
+//        mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
+//        assert mNotificationManager != null;
+//        mNotificationManager.createNotificationChannel(notificationChannel) ;
+//    }
+//    assert mNotificationManager != null;
+//
+//    mNotificationManager.notify(( int ) System. currentTimeMillis () , mBuilder.build()) ;
+//public void onMessageReceived(RemoteMessage remoteMessage) {
+//
+//    // Check if message contains a data payload.
+//    if (remoteMessage.getData().size() > 0) {
+//        showNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("author"));
+//    }
+//
+//    // Check if message contains a notification payload.
+//    if (remoteMessage.getNotification() != null) {
+//
+//    }
+//}
+//
+//    private void showNotification(String title, String author) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+//
+//        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setContentTitle("New Article: " + title)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setContentText("By " + author)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent);
+//
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+//    }
+
+    public  void widget() {
+//        txtDetection = (TextView) findViewById(R.id.txtDetection);
+    }
+}
